6a6d17d602ca5af572eb3767682106c9edf60bc5
Subash Chandra <subash.k3110@outlook.com>
2015-02-05 12:27:40 +0530

    added rehash method

--- 3_Hashing/1_seperateChaining.c
+++ 3_Hashing/1_seperateChaining.c
@@ -1,6 +1,6 @@
 //implement seperate chaining 
 
-#define LOAD_FACTOR 20 //per bucket max
+#define LOAD_FACTOR 3 //per bucket max
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -34,6 +34,10 @@ typedef struct hashTable HashTable;
 typedef HashTable *HashTableptr;
 
 
+int insert(HashTableptr h, int data);
+void print(HashTableptr h);
+
+
 //create a emoty hash table
 struct hashTable* create(int size)
 {
@@ -42,11 +46,9 @@ struct hashTable* create(int size)
 	if(h==NULL)
 		return NULL;
 
-	printf("check1\n");
 	h->tsize=size/LOAD_FACTOR; //
 	h->count=0;
 	h->table = (HashTableNodeptr *)malloc(sizeof(HashTableNodeptr)*(h->tsize)); //create a hashTable of size h->tsize
-	printf("check2\n");
 
 	if(h->table==NULL)
 	{
@@ -61,7 +63,6 @@ struct hashTable* create(int size)
 		h->table[i]->next=NULL;
 		h->table[i]->bcount=0;
 	}
-	printf("check3\n");
 
 	return h;
 }
@@ -90,6 +91,43 @@ int search(struct hashTable *h,int data)
 	return 0;
 }
 
+HashTableptr rehash(HashTableptr h)
+{
+
+	int oldTableSize = h->tsize*LOAD_FACTOR; //old table size = no of buckets * LOAD_FACTOR
+
+	Listnodeptr temp;
+	int i;
+	HashTableptr newTable = (HashTableptr)malloc(sizeof(HashTable));
+	newTable->count = 0;
+	newTable->tsize = (oldTableSize*2)/LOAD_FACTOR; //no of buckets in new table is (2* size of old table)/LOAD_FACTOR
+	newTable->table = (HashTableNodeptr *)malloc(sizeof(HashTableNodeptr)*newTable->tsize);
+
+	for(i=0;i<newTable->tsize;i++)
+	{
+		newTable->table[i]=(HashTableNodeptr)malloc(sizeof(HashTableNode));
+		newTable->table[i]->bcount=0;
+		newTable->table[i]->next=NULL;
+	}
+	
+	//now iterate over all teh old hashtable elemenst nd insert them in the newtable
+	for(i=0;i<h->tsize;i++)
+	{
+		temp=h->table[i]->next;
+		while(temp!=NULL)
+		{
+			insert(newTable,temp->data);
+			temp=temp->next;
+		}
+	}
+	
+	//delete odl table nd point h to new table
+	h=newTable;
+	printf("rehashed table\n");
+	return h;
+}
+
+
 
 //insert into the hash table
 int insert(HashTableptr h, int data)
@@ -117,9 +155,6 @@ int insert(HashTableptr h, int data)
 	h->table[index]->bcount++; //incremenet no fo elments in that bucket
 	h->count++; //no of elements in the table
 
-	//rehash check
-	//if(h->count/h->tsize > LOAD_FACTOR)
-	//	rehash(h);
 	return 1;
 }
 	
@@ -128,6 +163,8 @@ int insert(HashTableptr h, int data)
 //delete from teh hash table
 int delete(HashTableptr h, int data)
 {
+	if(h->count==0)
+		return 0;
 	int index;
 	Listnodeptr temp, pre;
 	index= hash(data, h->tsize);
@@ -139,16 +176,19 @@ int delete(HashTableptr h, int data)
 	{
 		if(temp->data==data) //if foudn delete nd return 1, else 0
 		{
-			if(pre!=NULL)
-				pre->next=temp->next;
+			if(pre==NULL)
+				h->table[index]->next=temp->next;
 			else
-				h->table[index]->next=NULL;
+				pre->next=temp->next;
 			free(temp);
 
 			h->table[index]->bcount--;
 			h->count--;
 			return 1;
 		}
+		else
+			pre=temp;
+			temp=temp->next;
 
 	}
 	return 0;
@@ -167,7 +207,7 @@ void print(HashTableptr h)
 	}
 
 	Listnodeptr cur;
-	printf("table size: %d\n",h->count);
+	printf("table size: %d buckets: %d\n",h->count,h->tsize);
 
 	int i;
 	for(i=0;i<h->tsize;i++) //iterate over the table
@@ -205,12 +245,23 @@ int main()
 			break;
 		insert(h,data);
 		print(h);
+		//rehash check
+		if((h->count/h->tsize) >= LOAD_FACTOR)
+		{
+			h=rehash(h);
+			print(h);
+		}
 	}
 
-	printf("enter data to delete: ");
-	scanf("%d",&data);
-	delete(h,data);
-	print(h);
+	while(1)
+	{
+		printf("enter data to delete: ");
+		scanf("%d",&data);
+		if(data==-1)
+			break;
+		delete(h,data);
+		print(h);
+	}
 
 
 
