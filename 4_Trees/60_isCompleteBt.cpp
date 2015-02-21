477171d3b2eb8c0f6089959039c80a246e3bb2e9
Subash Chandra <subash.k3110@outlook.com>
2015-02-21 20:46:57 +0530

    as of 21Feb15 preDinner

---
+++ 4_Trees/60_isCompleteBt.cpp
@@ -0,0 +1,184 @@
+//given a bt, check if its a complete bt or not
+
+
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+struct bstnode
+{
+	int data;
+	struct bstnode *left;
+	struct bstnode *right;
+};
+
+typedef struct bstnode Bstnode;
+typedef Bstnode *Bstnodeptr;
+
+
+Bstnodeptr insert(Bstnodeptr root, int data)
+{
+	Bstnodeptr newnode;
+
+	if(root==NULL) //if null 
+	{
+		newnode=(Bstnodeptr)malloc(sizeof(Bstnode));
+		newnode->data=data;
+		newnode->left=NULL;
+		newnode->right=NULL;
+		root = newnode;
+	}
+
+	else if(root->data == data)
+		printf("duplicate\n");
+
+	else if(data <root->data) //go recursive to the left subtree
+		root->left=insert(root->left,data);
+
+	else //go recursive to teh right subtree
+		root->right=insert(root->right,data);
+
+	return root;
+}
+
+Bstnodeptr max(Bstnodeptr root)
+{
+	if(root->right==NULL)
+		return root;
+
+	//else go recursive
+	return max(root->right); 
+}
+
+
+//delete node in the bst
+Bstnodeptr deletenode(Bstnodeptr root, int data)
+{
+	Bstnodeptr temp;
+	if(root==NULL)
+	{
+		printf("element not found\n");
+	}
+
+	else if(data<root->data) //go recursive on left child
+		root->left=deletenode(root->left,data);
+
+	else if(data>root->data) //right
+		root->right=deletenode(root->right,data);
+
+	else //data  foudn
+	{
+		if(root->left !=NULL && root->right !=NULL) //if node has two children
+		{
+			temp = max(root->left); //find max in the left child, copy it to root nd delete that node
+			root->data = temp->data;
+			root->left = deletenode(root->left,root->data);
+		}
+		else //only one child
+		{
+			temp=root;
+			if(root->left==NULL)
+				root=root->right;
+			else if(root->right==NULL)
+				root=root->left;
+			free(temp);
+		}
+	}
+	return root;
+}
+
+
+//inorder traversal of a tree
+void inorder(Bstnodeptr root)
+{
+	if(root==NULL)
+		return;
+	inorder(root->left);
+	printf("%d ",root->data);
+	inorder(root->right);
+}
+
+
+//preorder traversal of a tree
+void preorder(Bstnodeptr root)
+{
+	if(root==NULL)
+		return;
+	printf("%d ",root->data);
+	preorder(root->left);
+	preorder(root->right);
+}
+
+//postorder traversal of a tree
+void postorder(Bstnodeptr root)
+{
+	if(root==NULL)
+		return;
+	postorder(root->left);
+	postorder(root->right);
+	printf("%d ",root->data);
+}
+
+
+
+int isComplete(Bstnodeptr root)
+{
+	int flag=0;
+	if(root==NULL)
+		return 1;
+
+	queue<Bstnodeptr>q1;
+	q1.push(root);
+
+	while(q1.size()!=0)
+	{
+		root=q1.front();
+		q1.pop();
+		if(root->left) //if left child
+		{
+			if(flag==1) //check if flag is already set
+				return 0;
+			q1.push(root->left);
+		}
+		else //set flag
+			flag=1;
+
+		if(root->right)
+		{
+			if(flag==1) //check if flag is alrady set, if so return false
+				return 0;
+			q1.push(root->right);
+		}
+		else
+			flag=1;
+	}
+	return 1;
+}
+
+	
+
+int main()
+{
+	Bstnodeptr root = NULL;
+
+	int data;	
+	printf("enter data or -1 to break ");
+	while(1)
+	{
+		scanf("%d",&data);
+		if(data==-1)
+			break;
+
+		root=insert(root,data);
+	}
+	printf("preorder: ");
+	preorder(root);
+	printf("\n");
+
+	printf("%s\n",isComplete(root)?"is complete":"is not complete");
+
+	return 0;
+}
